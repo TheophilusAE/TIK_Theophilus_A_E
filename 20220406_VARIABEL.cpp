ed86ca9ee2dfdfa0c93e0ee810993b1656687f7a
TheophilusAE <103085737+TheophilusAE@users.noreply.github.com>
2022-04-05 20:08:47 -0700

    Create 20220406_VARIABEL.cpp

---
+++ 20220406_VARIABEL.cpp
@@ -0,0 +1,18 @@
+//header
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{//begin
+   
+    float a;
+    float b;
+    string nama; // deklarasi variabel
+    a = 10.21;
+    b = 3.14;
+    nama = "Theophilus"; // masukan valea
+    cout << nama << endl; // menampilkan variabel
+    cout << a + b <<endl;
+    
+}//end 
